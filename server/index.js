0202930d4573bdabc982978021b37467c6c4c0eb
Shelby Webb <shelbylyn.webb@gmail.com>
2022-05-21 15:39:36 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const {
+    getHouses,
+    deleteHouse, 
+    createHouse,
+    updateHouse
+} = require('./controller');
+
+app.get(`/api/houses`, getHouses);
+app.delete(`/api/houses/:id`, deleteHouse);
+app.post(`/api/houses`, createHouse);
+app.put(`/api/houses/:id`, updateHouse);
+
+
+app.listen(4001, () => console.log('Server running on port 4001'));
